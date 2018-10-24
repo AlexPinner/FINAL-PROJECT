916ac45e82d89a6318d1ce2d9776aaed778bc317
AlexPinner <31456101+AlexPinner@users.noreply.github.com>
2018-10-24 13:34:09 -0400

    Update Tkinter Gui Test.py
    Listbox, scrollbar, menu, and spinbox

--- Tkinter Gui Test.py
+++ Tkinter Gui Test.py
@@ -22,5 +22,35 @@ root = Tk()
 
 app = App(root)
 
+listbox = Listbox(root)
+listbox.pack()
+
+listbox.insert(END, "a list entry")
+
+for item in ["one", "two", "three", "four"]:
+    listbox.insert(END, item)
+
+scrollbar = Scrollbar(root)
+
+listbox = Listbox(root, yscrollcommand=scrollbar.set)
+for i in range(1000):
+    listbox.insert(END, str(i))
+listbox.pack(side=LEFT, fill=BOTH)
+scrollbar.pack(side=LEFT, fill=Y)
+
+scrollbar.config(command=listbox.yview)
+
+def hello():
+    print("hello!")
+
+menubar = Menu(root)
+menubar.add_command(label="Hello!", command=hello)
+menubar.add_command(label="Quit!", command=root.quit)
+
+# display the menu
+root.config(menu=menubar)
+
+w = Spinbox(root, from_=0, to=10)
+w.pack()
+
 root.mainloop()
-root.destroy()
