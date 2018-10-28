0e1a480f25e7aed7c88fa724cc6a026fb99e042b
AlexPinner <31456101+AlexPinner@users.noreply.github.com>
2018-10-28 05:08:12 -0400

    Update Tkinter Gui Test.py
    Pane testing

--- Tkinter Gui Test.py
+++ Tkinter Gui Test.py
@@ -1,56 +1,16 @@
 from tkinter import *
 
-class App:
+my_window = Tk()
 
-    def __init__(self, master):
+blue_frame= Frame(my_window,bg='Blue')
 
-        frame = Frame(master)
-        frame.pack()
+m = PanedWindow(orient=HORIZONTAL)
+m.pack(fill=BOTH, expand=1)
 
-        self.button = Button(
-            frame, text="QUIT", fg="red", command=frame.quit
-            )
-        self.button.pack(side=LEFT)
+top = Label(m, text="left pane")
+m.add(blue_frame)
 
-        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
-        self.hi_there.pack(side=LEFT)
+bottom = Label(m, text="right pane")
+m.add(bottom)
 
-    def say_hi(self):
-        print( "hi there, everyone!")
-
-root = Tk()
-
-app = App(root)
-
-listbox = Listbox(root)
-listbox.pack()
-
-listbox.insert(END, "a list entry")
-
-for item in ["one", "two", "three", "four"]:
-    listbox.insert(END, item)
-
-scrollbar = Scrollbar(root)
-
-listbox = Listbox(root, yscrollcommand=scrollbar.set)
-for i in range(1000):
-    listbox.insert(END, str(i))
-listbox.pack(side=LEFT, fill=BOTH)
-scrollbar.pack(side=LEFT, fill=Y)
-
-scrollbar.config(command=listbox.yview)
-
-def hello():
-    print("hello!")
-
-menubar = Menu(root)
-menubar.add_command(label="Hello!", command=hello)
-menubar.add_command(label="Quit!", command=root.quit)
-
-# display the menu
-root.config(menu=menubar)
-
-w = Spinbox(root, from_=0, to=10)
-w.pack()
-
-root.mainloop()
+mainloop()
